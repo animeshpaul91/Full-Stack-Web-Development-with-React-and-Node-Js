1970727e8bd59269ce712f325ce0980666248fc6
Animesh Paul <ani.nitmz@gmail.com>
2020-09-18 17:25:46 -0600

    Task-3 Completed

--- FE_Web_Development_with_React/Week-3/React/confusion/src/components/DishDetailComponent.js
+++ FE_Web_Development_with_React/Week-3/React/confusion/src/components/DishDetailComponent.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import {
-        Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem,
-        Button, Modal, ModalHeader, ModalBody, Row, Col, Label
-       } from 'reactstrap';
+    Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem,
+    Button, Modal, ModalHeader, ModalBody, Row, Col, Label
+} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
