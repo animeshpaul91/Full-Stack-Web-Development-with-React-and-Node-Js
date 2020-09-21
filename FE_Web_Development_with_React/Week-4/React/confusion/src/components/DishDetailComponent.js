23f59bdfe622f558ba2a2fd697befcd36e73f80c
Animesh Paul <ani.nitmz@gmail.com>
2020-09-20 20:41:52 -0600

    Added Card Animations

--- FE_Web_Development_with_React/Week-4/React/confusion/src/components/DishDetailComponent.js
+++ FE_Web_Development_with_React/Week-4/React/confusion/src/components/DishDetailComponent.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -29,8 +30,6 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        // console.log('Current State is: ' + JSON.stringify(values));
-        // alert('Current State is: ' + JSON.stringify(values));
         this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
@@ -119,13 +118,19 @@ class CommentForm extends Component {
 function RenderDish({ dish }) {
     return (
         <div className="col-12 col-md-5 m-1">
-            <Card>
-                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         </div>
     )
 }
@@ -133,26 +138,22 @@ function RenderDish({ dish }) {
 function RenderComments({ comments, postComment, dishId }) {
     // console.log(comments)
     if (comments != null) {
-
-        let list = comments.map((comments) => {
-
-            return (
-                <li key={comments.id} >
-                    <div>
-                        <p>{comments.comment}</p>
-                        <p>--{comments.author},
-                            {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comments.date)))}</p>
-                    </div>
-                </li>
-
-            )
-        })
-
         return (
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
                 <ul className="list-unstyled">
-                    {list}
+                <Stagger in>
+                    {comments.map((comment) => {
+                        return (
+                            <Fade in>
+                            <li key={comment.id}>
+                            <p>{comment.comment}</p>
+                            <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                            </li>
+                            </Fade>
+                        );
+                    })}
+                    </Stagger>
                 </ul>
                 <CommentForm dishId={dishId} postComment={postComment} />
             </div>
@@ -192,7 +193,7 @@ const DishDetail = (props) => {
                         <BreadcrumbItem>
                             <Link to="/menu">Menu</Link>
                         </BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
                     </Breadcrumb>
                     <div className="col-12">
                         <h3>{props.dish.name}</h3>
