30034422dd9b25282c28332ed1d84f123aa60bb8
Animesh Paul <ani.nitmz@gmail.com>
2021-05-08 13:02:07 -0600

    Completed Basic Node Js Module

--- NodeJs_Basics/app.js
+++ NodeJs_Basics/app.js
@@ -3,7 +3,13 @@ const routes = require("./routes");
 
 console.log(routes.someText);
 
-const server = http.createServer(routes.handler); // executes the function in routes that handles incoming requests
+/* Node Js runs non-blocking JS code and uses an event-driven approach for running logic in code.
+   A standard node program exits as soon as there is no more work to do
+   The createServer() event never finishes by default
+   Event Loop uses callbacks
+*/
+
+const server = http.createServer(routes.handler); // creates an infinite event loop (listener)
 
 
 server.listen(3000);
