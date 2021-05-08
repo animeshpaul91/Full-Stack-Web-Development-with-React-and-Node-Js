12171df2528c1f84c976adfe1e5ca92cbc758085
Animesh Paul <ani.nitmz@gmail.com>
2021-05-08 11:37:47 -0600

    Parsed Request Body

--- NodeJs_Basics/app.js
+++ NodeJs_Basics/app.js
@@ -1,8 +1,38 @@
 const http = require("http");
+const fs = require('fs');
 
 // this program keeps on running as long as there are event listeners registered
 const server = http.createServer((request, response) => { // this is an event listener which never stops unless told to do so
-    console.log(request.url, request.method, request.headers);
+    const url = request.url;
+    const method = request.method;
+
+    if (url === "/") { // url is true and is exactly "/"
+        response.setHeader("Content-Type", "text/html");
+        response.write("<html>");
+        response.write("<head> <title> Enter Message </title> </head>");
+        response.write('<body> <form action="/message" method="POST"> <input type="text" name="message"> <button type="submit"> Send </button> </form> </body>');
+        response.write("</html>")
+        return response.end();
+    }
+
+    if (url === "/message" && method === "POST") {
+        const body = [];
+        request.on('data', (chunk) => { // register an event listener
+            body.push(chunk); // get all chunks from stream
+        });
+
+        request.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split("=")[1];
+            fs.writeFileSync("message.txt", message);
+        });
+        
+        response.statusCode = 302;
+        response.setHeader('Location', '/'); // redirect to "/"
+        return response.end();
+    }
+
     // process.exit(); // will exit upon receiving first request
     // fill response object with data
     response.setHeader("Content-Type", "text/html");
