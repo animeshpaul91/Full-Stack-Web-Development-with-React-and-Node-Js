b97e64d6cc2f0c9f55face5faff1474fc9c8783c
Animesh Paul <ani.nitmz@gmail.com>
2021-05-08 12:46:23 -0600

    Refactored Code

--- NodeJs_Basics/app.js
+++ NodeJs_Basics/app.js
@@ -1,47 +1,9 @@
 const http = require("http");
-const fs = require('fs');
+const routes = require("./routes");
 
-// this program keeps on running as long as there are event listeners registered
-const server = http.createServer((request, response) => { // this is an event listener which never stops unless told to do so
-    const url = request.url;
-    const method = request.method;
+console.log(routes.someText);
 
-    if (url === "/") { // url is true and is exactly "/"
-        response.setHeader("Content-Type", "text/html");
-        response.write("<html>");
-        response.write("<head> <title> Enter Message </title> </head>");
-        response.write('<body> <form action="/message" method="POST"> <input type="text" name="message"> <button type="submit"> Send </button> </form> </body>');
-        response.write("</html>")
-        return response.end();
-    }
+const server = http.createServer(routes.handler); // executes the function in routes that handles incoming requests
 
-    if (url === "/message" && method === "POST") {
-        const body = [];
-        request.on('data', (chunk) => { // register an event listener
-            body.push(chunk); // get all chunks from stream
-        });
-
-        return request.on('end', () => { // register an end event to incoming data on stream
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message = parsedBody.split("=")[1];
-            fs.writeFile("message.txt", message, error => {
-                response.statusCode = 302;
-                response.setHeader('Location', '/'); // redirect to "/"
-                return response.end();
-            });
-        });                
-    }
-
-    // process.exit(); // will exit upon receiving first request
-    // fill response object with data
-    response.setHeader("Content-Type", "text/html");
-    response.write("<html>");
-    response.write("<head> <title> My First Page </title> </head>");
-    response.write("<body><h1> Hello from Node Js Server! </h1> </body>");
-    response.write("</html>")
-    response.end();
-    // cannot write to response anymore at this point
-});
 
 server.listen(3000);
