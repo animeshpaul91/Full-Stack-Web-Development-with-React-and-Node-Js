80d5332960677ef2791b627a95aefb8e95ed09c4
Animesh Paul <ani.nitmz@gmail.com>
2021-05-12 17:11:46 -0600

    Added 404 Response

--- Express Js/app.js
+++ Express Js/app.js
@@ -8,12 +8,16 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+app.use(bodyParser.urlencoded({extended: false})); // registers a middlewate and calls next()
+
 app.use(adminRoutes); // router exported in admin.js is a valid midddleware function
 // This will add all admin routes
 // The order matters
 
-app.use(bodyParser.urlencoded({extended: false})); // registers a middlewate and calls next()
-
 app.use(shopRoutes);
+
+app.use((request, response, next) => {
+   response.status(404).send('<h1> Page not found </h1>');
+});
 
 app.listen(3000);
